01182850b23be3a1557ef82719fcd2b0222fcb38
nadia <nadanikonova50310@gmail.com>
2024-07-19 20:19:48 +0300

    Initial

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
-from funcs import get_transaction, load_data
-import transaction as t
+from functions import (get_json_file,
+                       filtered_lines,
+                       sorted_datetime_toprows,
+                       result_transactions)
+import os
+from config import ROOT_DIR
+
+file_path = os.path.join(ROOT_DIR, 'data', 'operations.json')
+how_many_rows = 5
 
 
 def main():
-    last_executed_transactions = get_transaction(load_data('operations.json'))
-    for trans in last_executed_transactions:
-        t.Transactions(trans)
-        print(t.Transactions(trans), '\n')
+    content_to_filter = get_json_file(file_path)
+    filtered_array = filtered_lines(content_to_filter)
+    filtered_top5rows = sorted_datetime_toprows(filtered_array, how_many_rows)
+    row_ = result_transactions(filtered_top5rows)
+    print(row_)
 
 
-if __name__ == '__main__':
-    main()
+main()
